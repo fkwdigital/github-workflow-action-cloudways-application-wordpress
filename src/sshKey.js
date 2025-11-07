2be80cff26729f79c05e41ee08ed73f8cb15d061
Christina Gleason <git@finarina.com>
2025-11-07 17:44:09 -0500

    Adjusts ssh connection logic

--- src/sshKey.js
+++ src/sshKey.js
@@ -1,5 +1,6 @@
 const { writeFileSync } = require('fs');
 const { join } = require('path');
+
 const { validateDir, validateFile } = require('./helpers');
 
 const { HOME } = process.env;
@@ -9,11 +10,13 @@ const addSshKey = (key, name) => {
   const filePath = join(sshDir, name);
 
   validateDir(sshDir);
-
   validateFile(`${sshDir}/known_hosts`);
 
   try {
-    writeFileSync(filePath, key, { encoding: 'utf8', mode: 0o600 });
+    writeFileSync(filePath, key, {
+      encoding: 'utf8',
+      mode: 0o600,
+    });
   } catch (e) {
     console.error('⚠️ writeFileSync error', filePath, e.message);
     process.abort();
@@ -24,4 +27,6 @@ const addSshKey = (key, name) => {
   return filePath;
 };
 
-module.exports = { addSshKey };
+module.exports = {
+  addSshKey,
+};
